e503f4e856538b095d2d63eddcc4a0dc1267776c
Subramanya M <sukruth21@gmail.com>
2021-06-14 21:58:05 +0530

    Delete METROSYSTEMJDBC directory

--- METROSYSTEMJDBC/src/com/station/pesistence/StationDao.java
+++
@@ -1,7 +0,0 @@
-package com.station.pesistence;
-
-import java.sql.SQLException;
-
-public interface StationDao {
-	boolean addStation(String stationName) throws ClassNotFoundException, SQLException;
-}
--- METROSYSTEMJDBC/src/com/station/service/StationService.java
+++
@@ -1,7 +0,0 @@
-package com.station.service;
-
-import java.sql.SQLException;
-
-public interface StationService {
-	boolean addStation(String stationName) throws ClassNotFoundException, SQLException;
-}
